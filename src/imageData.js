96c921d3a52e096c1cba78e9a1ed0f883ca029ab
mrjohnming <john.ngai@hotmail.com>
2022-07-19 17:19:48 -0400

    Create mock image data

---
+++ src/imageData.js
@@ -0,0 +1,82 @@
+// Photos
+import paulaner from './photos/img01.jpg';
+import benz from './photos/img02.jpg';
+
+// Layouts
+const PORTRAIT = 'portrait';
+const LANDSCAPE = 'landscape';
+
+const imageData = [
+  {
+    id: 1,
+    title: 'Paulaner Hefe-Weissbier 500 ml',
+    description: 'Sample description',
+    layout: PORTRAIT,
+    source: paulaner,
+  },
+  {
+    id: 2,
+    title: '2020 Mercedes-Benz S53 AMG',
+    description: 'Sample description',
+    layout: LANDSCAPE,
+    source: benz,
+  },
+  {
+    id: 3,
+    title: 'Paulaner Hefe-Weissbier 500 ml',
+    description: 'Sample description',
+    layout: PORTRAIT,
+    source: paulaner,
+  },
+  {
+    id: 4,
+    title: '2020 Mercedes-Benz S53 AMG',
+    description: 'Sample description',
+    layout: LANDSCAPE,
+    source: benz,
+  },
+  {
+    id: 5,
+    title: 'Paulaner Hefe-Weissbier 500 ml',
+    description: 'Sample description',
+    layout: PORTRAIT,
+    source: paulaner,
+  },
+  {
+    id: 6,
+    title: '2020 Mercedes-Benz S53 AMG',
+    description: 'Sample description',
+    layout: LANDSCAPE,
+    source: benz,
+  },
+  {
+    id: 7,
+    title: 'Paulaner Hefe-Weissbier 500 ml',
+    description: 'Sample description',
+    layout: PORTRAIT,
+    source: paulaner,
+  },
+  {
+    id: 8,
+    title: '2020 Mercedes-Benz S53 AMG',
+    description: 'Sample description',
+    layout: LANDSCAPE,
+    source: benz,
+  },
+  {
+    id: 9,
+    title: 'Paulaner Hefe-Weissbier 500 ml',
+    description: 'Sample description',
+    layout: PORTRAIT,
+    source: paulaner,
+  },
+  {
+    id: 10,
+    title: '2020 Mercedes-Benz S53 AMG',
+    description: 'Sample description',
+    layout: LANDSCAPE,
+    source: benz,
+  },
+];
+
+export default imageData;
