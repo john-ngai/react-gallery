acbd89614ba9b7223756ac86b9213ec3f5f78c81
mrjohnming <john.ngai@hotmail.com>
2022-07-19 17:21:31 -0400

    Create galleryListElements using mock image data

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -1,9 +1,21 @@
+import imageData from '../imageData';
+import GalleryListItem from './GalleryListItem';
 import './GalleryList.scss';
 
 export default function GalleryList() {
+  const galleryListElements = imageData.map(img => (
+    <GalleryListItem
+      key={img.id}
+      title={img.title}
+      description={img.description}
+      layout={img.layout}
+      source={img.source}
+    />
+  ));
+  
   return (
     <div className='container GalleryList'>
-      
+      {galleryListElements}
     </div>
   );
 }
