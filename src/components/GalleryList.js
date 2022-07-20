7879bb0f7553012070cab5efd07c7966e9992a50
mrjohnming <john.ngai@hotmail.com>
2022-07-20 19:05:48 -0400

    Refactor property values based on updated image data structure

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -1,21 +1,26 @@
+// Data
 import imageData from '../imageData';
+// Component
 import GalleryListItem from './GalleryListItem';
+// Stylesheet
 import './GalleryList.scss';
 
-export default function GalleryList() {
-  const galleryListElements = imageData.map(img => (
+export default function GalleryList({ setSelectedListItem }) {
+  const galleryListElements = Object.keys(imageData).map(id => (
     <GalleryListItem
-      key={img.id}
-      title={img.title}
-      description={img.description}
-      layout={img.layout}
-      source={img.source}
+      key={imageData[id].id}
+      id={imageData[id].id}
+      title={imageData[id].title}
+      description={imageData[id].description}
+      layout={imageData[id].layout}
+      source={imageData[id].source}
+      setSelectedListItem={setSelectedListItem}
     />
   ));
   
   return (
     <div className='container GalleryList'>
-      {galleryListElements}
+        {galleryListElements}
     </div>
   );
 }
