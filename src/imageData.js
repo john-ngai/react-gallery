5e5d495808d5feb06440d173688fea169dd17e50
mrjohnming <john.ngai@hotmail.com>
2022-07-20 19:13:37 -0400

    Change data structure from an array to object, and remove the layout property

--- src/imageData.js
+++ src/imageData.js
@@ -1,82 +1,67 @@
-// Photos
 import paulaner from './photos/img01.jpg';
 import benz from './photos/img02.jpg';
 
-// Layouts
-const PORTRAIT = 'portrait';
-const LANDSCAPE = 'landscape';
-
-const imageData = [
-  {
+const imageData = {
+  1: {
     id: 1,
     title: 'Paulaner Hefe-Weissbier 500 ml',
     description: 'Sample description',
-    layout: PORTRAIT,
     source: paulaner,
   },
-  {
+  2: {
     id: 2,
     title: '2020 Mercedes-Benz S53 AMG',
     description: 'Sample description',
-    layout: LANDSCAPE,
     source: benz,
   },
-  {
+  3: {
     id: 3,
     title: 'Paulaner Hefe-Weissbier 500 ml',
     description: 'Sample description',
-    layout: PORTRAIT,
     source: paulaner,
   },
-  {
+  4: {
     id: 4,
     title: '2020 Mercedes-Benz S53 AMG',
     description: 'Sample description',
-    layout: LANDSCAPE,
     source: benz,
   },
-  {
+  5: {
     id: 5,
     title: 'Paulaner Hefe-Weissbier 500 ml',
     description: 'Sample description',
-    layout: PORTRAIT,
     source: paulaner,
   },
-  {
+  6: {
     id: 6,
     title: '2020 Mercedes-Benz S53 AMG',
     description: 'Sample description',
-    layout: LANDSCAPE,
     source: benz,
   },
-  {
+  7: {
     id: 7,
     title: 'Paulaner Hefe-Weissbier 500 ml',
     description: 'Sample description',
-    layout: PORTRAIT,
     source: paulaner,
   },
-  {
+  8: {
     id: 8,
     title: '2020 Mercedes-Benz S53 AMG',
     description: 'Sample description',
-    layout: LANDSCAPE,
     source: benz,
   },
-  {
+  9: {
     id: 9,
     title: 'Paulaner Hefe-Weissbier 500 ml',
     description: 'Sample description',
-    layout: PORTRAIT,
     source: paulaner,
   },
-  {
+  10: {
     id: 10,
     title: '2020 Mercedes-Benz S53 AMG',
     description: 'Sample description',
-    layout: LANDSCAPE,
     source: benz,
   },
-];
+};
 
 export default imageData;
