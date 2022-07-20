97b175692ef0906bced7e85295699248224a9c0f
mrjohnming <john.ngai@hotmail.com>
2022-07-20 19:04:17 -0400

    Render instance of <ImageModal />

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,24 @@
+// Package
+import { useState } from 'react';
+// Components
 import GalleryList from './GalleryList';
+import ImageModal from './ImageModal';
+// Stylesheet
 import './Gallery.scss';
 
-export default function() {
+export default function Gallery() {
+  const [selectedListItem, setSelectedListItem] = useState(null);
+  
   return (
-    <section className='container Gallery'>
-      <GalleryList />
+    <section className='container Gallery'> 
+      <GalleryList setSelectedListItem={setSelectedListItem} />
+      {
+        selectedListItem && 
+        <ImageModal
+          selectedListItem={selectedListItem}
+          setSelectedListItem={setSelectedListItem}
+        />
+      }
     </section>
   );
 }
