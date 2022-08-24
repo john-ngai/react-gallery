e298d9b193eddeebd8eec617f1296b1a89e51f04
mrjohnming <john.ngai@hotmail.com>
2022-08-24 17:21:03 -0400

    Remove onClick handler from img

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,21 +1,24 @@
-import './ImageModal.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import "./ImageModal.scss";
 
 export default function ImageModal(props) {
   const { selectedListItem, setSelectedListItem } = props;
   const { title, description, source } = selectedListItem;
-  
+
   // Disable scrolling when this component is mounted.
-  document.body.style.overflow = 'hidden';
-  
+  document.body.style.overflow = "hidden";
+
   return (
-    <div className='container ImageModal'
+    <div
+      className="container ImageModal"
       onClick={() => setSelectedListItem(null)}
     >
       <img
         src={source}
         alt={title}
-        onClick={event => event.stopPropagation()}
       />
+      {/* <FontAwesomeIcon icon={solid("mug-saucer")} /> */}
     </div>
   );
 }
