30c20a0d09cb280ac23cac460c04cf8f0ef001b3
mrjohnming <john.ngai@hotmail.com>
2022-08-29 11:34:34 -0400

    Add & style Font Awesome icons - chevron-left & chevron-right

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,19 +1,24 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid/*, regular*/ } from "@fortawesome/fontawesome-svg-core/import.macro";
-import "./ImageModal.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  solid /*, regular*/,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
+import './ImageModal.scss';
 
 export default function ImageModal(props) {
   const { selectedListItem, setSelectedListItem } = props;
-  const { title/*, description*/, source } = selectedListItem;
+  const { title /*, description*/, source } = selectedListItem;
 
   return (
-    <div
-      className="container ImageModal"
-    >
+    <div className="container ImageModal">
       <FontAwesomeIcon
         className="xmark"
-        icon={solid("xmark")}
+        icon={solid('xmark')}
         onClick={() => setSelectedListItem(null)}
+      />
+      <FontAwesomeIcon className="chevron-left" icon={solid('chevron-left')} />
+      <FontAwesomeIcon
+        className="chevron-right"
+        icon={solid('chevron-right')}
       />
       <img src={source} alt={title} />
     </div>
