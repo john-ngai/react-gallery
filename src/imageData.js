ea06216158641ac0c9c75ea5352d64f9ae9d9a5e
mrjohnming <john.ngai@hotmail.com>
2022-08-29 10:54:33 -0400

    Add more images

--- src/imageData.js
+++ src/imageData.js
@@ -1,66 +1,60 @@
 import paulaner from './photos/img01.jpg';
 import benz from './photos/img02.jpg';
+import lionKing from './photos/img03.jpg';
+import vintageBenz from './photos/img04.jpg';
+import gClass from './photos/img05.jpg';
+import oysters from './photos/img06.jpg';
+import connor from './photos/img07.jpg';
+import yoyo from './photos/img08.jpg';
 
 const imageData = {
   1: {
     id: 1,
     title: 'Paulaner Hefe-Weissbier 500 ml',
-    description: 'Sample description',
+    description: 'Best tasting beer',
     source: paulaner,
   },
   2: {
     id: 2,
     title: '2020 Mercedes-Benz S53 AMG',
-    description: 'Sample description',
+    description: '',
     source: benz,
   },
   3: {
     id: 3,
-    title: 'Paulaner Hefe-Weissbier 500 ml',
-    description: 'Sample description',
-    source: paulaner,
+    title: 'Lion King',
+    description: 'Lion King on Broadway in New York City',
+    source: lionKing,
   },
   4: {
     id: 4,
-    title: '2020 Mercedes-Benz S53 AMG',
-    description: 'Sample description',
-    source: benz,
+    title: 'Vintage 1980 Mercedes-Benz 300 SD',
+    description: 'Painted by Tom Forrestall',
+    source: vintageBenz,
   },
   5: {
     id: 5,
-    title: 'Paulaner Hefe-Weissbier 500 ml',
-    description: 'Sample description',
-    source: paulaner,
+    title: '2020 Mercedes Benz G-Class',
+    description: '',
+    source: gClass,
   },
   6: {
     id: 6,
-    title: '2020 Mercedes-Benz S53 AMG',
-    description: 'Sample description',
-    source: benz,
+    title: 'Kusshi west coast oysters',
+    description: 'Supplied by Daily Seafood based in Toronto',
+    source: oysters,
   },
   7: {
     id: 7,
-    title: 'Paulaner Hefe-Weissbier 500 ml',
-    description: 'Sample description',
-    source: paulaner,
+    title: 'Baby Connor',
+    description: '',
+    source: connor,
   },
   8: {
     id: 8,
-    title: '2020 Mercedes-Benz S53 AMG',
-    description: 'Sample description',
-    source: benz,
-  },
-  9: {
-    id: 9,
-    title: 'Paulaner Hefe-Weissbier 500 ml',
-    description: 'Sample description',
-    source: paulaner,
-  },
-  10: {
-    id: 10,
-    title: '2020 Mercedes-Benz S53 AMG',
-    description: 'Sample description',
-    source: benz,
+    title: 'Yoyo, the French Bulldog',
+    description: '',
+    source: yoyo,
   },
 };
 
