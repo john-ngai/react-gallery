dbbb48b1034a737f1e62c3cfa40c769060132de7
mrjohnming <john.ngai@hotmail.com>
2022-08-29 11:19:19 -0400

    Disable scrolling when an image is opened in the modal

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,17 +8,23 @@ import './Gallery.scss';
 
 export default function Gallery() {
   const [selectedListItem, setSelectedListItem] = useState(null);
-  
+
+  // Disable scrolling when an image is selected / opened in the modal.
+  if (selectedListItem) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'visible';
+  }
+
   return (
-    <section className='container Gallery'> 
+    <section className="container Gallery">
       <GalleryList setSelectedListItem={setSelectedListItem} />
-      {
-        selectedListItem && 
+      {selectedListItem && (
         <ImageModal
           selectedListItem={selectedListItem}
           setSelectedListItem={setSelectedListItem}
         />
-      }
+      )}
     </section>
   );
 }
