8cea6d02e221cf2c45cdf33909c938fd2c633a2f
mrjohnming <john.ngai@hotmail.com>
2022-09-02 09:11:42 -0400

    Remove legacy 'layout' prop from <GalleryListItem />

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -9,7 +9,6 @@ export default function GalleryList({ setModalImage }) {
       id={imageData[id].id}
       title={imageData[id].title}
       description={imageData[id].description}
-      layout={imageData[id].layout}
       source={imageData[id].source}
       setModalImage={setModalImage}
     />
